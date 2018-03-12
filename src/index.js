8be8f160d37fcef4221a09d9a646ecf938ae2d26
Rob Crocombe <rjwc1993@yahoo.co.uk>
2018-03-12 21:00:23 +0000

    Convert functions to classes, delete old files

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ const fs = require('fs');
 const glob = require('globby');
 const Generator = require('./generator');
 
-async function run(input) {
-  const paths = await glob(input);
+class PrettyHtml {
+  async run(input) {
+    const paths = await glob(input);
 
-  for (let i = 0; i < paths.length; ++i) {
-    fs.readFile(paths[i], 'utf8', (err, contents) => {
-      if (err) throw err;
+    for (let i = 0; i < paths.length; ++i) {
+      fs.readFile(paths[i], 'utf8', (err, contents) => {
+        if (err) throw err;
 
-      console.log(paths[i]);
+        console.log(paths[i]);
 
-      new Generator().run(paths[i], contents);
-    });
+        new Generator().run(paths[i], contents);
+      });
+    }
   }
 }
 
-module.exports = {
-  run,
-};
+module.exports = PrettyHtml;
